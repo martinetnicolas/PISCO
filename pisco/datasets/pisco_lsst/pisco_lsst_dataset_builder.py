e8dd3fd20517cd46eec7760e3917f679435e4046
Francois Lanusse <fr.eiffel@gmail.com>
2023-02-17 00:58:27 +0100

    fix pickle issue

--- pisco/datasets/pisco_lsst/pisco_lsst_dataset_builder.py
+++ pisco/datasets/pisco_lsst/pisco_lsst_dataset_builder.py
@@ -3,6 +3,64 @@ import numpy as np
 import tensorflow_datasets as tfds
 
 
+def _get_trial(trial):
+  from descwl_shear_sims.sim import make_sim, get_se_dim
+  from descwl_shear_sims.galaxies import FixedGalaxyCatalog, WLDeblendGalaxyCatalog
+  from descwl_shear_sims.stars import StarCatalog
+  from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf
+
+  seed = 9137
+  coadd_dim = 214
+  buff = 0
+
+  rng = np.random.RandomState(trial+seed)
+  # Randomly generate shear data
+  g1 = rng.uniform(low=-0.1, high=0.1)
+  g2 = rng.uniform(low=-0.1, high=0.1)
+  
+  galaxy_catalog = WLDeblendGalaxyCatalog(
+      rng=rng,
+      coadd_dim=coadd_dim,
+      buff=buff,
+  )
+
+  star_catalog = StarCatalog(
+      rng=rng,
+      coadd_dim=coadd_dim,
+      buff=buff,
+  )
+
+  # make a fixed psf, this assumes for instance a metacalibrated image
+  psf = make_fixed_psf(psf_type='gauss')
+
+  # generate some simulation data, with a particular shear,
+  # and dithering, rotation, cosmic rays, bad columns, star bleeds
+  # turned on.  By sending the star catalog we generate stars and
+  # some can be saturated and bleed
+
+  sim_data = make_sim(
+      rng=rng,
+      galaxy_catalog=galaxy_catalog,
+      star_catalog=star_catalog,
+      coadd_dim=coadd_dim,
+      g1=g1,
+      g2=g2,
+      psf=psf,
+      psf_dim=51,
+      bands=['r', 'i', 'z'],
+      noise_factor=0.58,
+      cosmic_rays=True,
+      bad_columns=True,
+      star_bleeds=True,
+  )
+
+  # Extract the center part of the image 
+  image = np.stack([sim_data['band_data'][b][0].image.array for b in ['r','i','z']], axis=-1)
+
+  return trial, {'image':image, 
+                'g':np.array([g1,g2]).astype(np.float32)}
+
+
 class Builder(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for pisco_lsst dataset."""
 
@@ -42,66 +100,11 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     import os
     from multiprocessing import Pool, cpu_count
     os.environ['CATSIM_DIR'] = str(path)+'/catsim'
-    from descwl_shear_sims.sim import make_sim, get_se_dim
-    from descwl_shear_sims.galaxies import FixedGalaxyCatalog, WLDeblendGalaxyCatalog
-    from descwl_shear_sims.stars import StarCatalog
-    from descwl_shear_sims.psfs import make_fixed_psf, make_ps_psf
 
-    seed = 9137
     # get the number of logical cpu cores
     n_cores = cpu_count()
     pool = Pool(processes=n_cores)
     ntrial = 10_000
-    coadd_dim = 214
-    buff = 0
-
-    def _get_trial(trial):
-      rng = np.random.RandomState(trial+seed)
-      # Randomly generate shear data
-      g1 = rng.uniform(low=-0.1, high=0.1)
-      g2 = rng.uniform(low=-0.1, high=0.1)
-      
-      galaxy_catalog = WLDeblendGalaxyCatalog(
-          rng=rng,
-          coadd_dim=coadd_dim,
-          buff=buff,
-      )
-
-      star_catalog = StarCatalog(
-          rng=rng,
-          coadd_dim=coadd_dim,
-          buff=buff,
-      )
-
-      # make a fixed psf, this assumes for instance a metacalibrated image
-      psf = make_fixed_psf(psf_type='gauss')
-
-      # generate some simulation data, with a particular shear,
-      # and dithering, rotation, cosmic rays, bad columns, star bleeds
-      # turned on.  By sending the star catalog we generate stars and
-      # some can be saturated and bleed
-
-      sim_data = make_sim(
-          rng=rng,
-          galaxy_catalog=galaxy_catalog,
-          star_catalog=star_catalog,
-          coadd_dim=coadd_dim,
-          g1=g1,
-          g2=g2,
-          psf=psf,
-          psf_dim=51,
-          bands=['r', 'i', 'z'],
-          noise_factor=0.58,
-          cosmic_rays=True,
-          bad_columns=True,
-          star_bleeds=True,
-      )
-
-      # Extract the center part of the image 
-      image = np.stack([sim_data['band_data'][b][0].image.array for b in ['r','i','z']], axis=-1)
-
-      return trial, {'image':image, 
-                    'g':np.array([g1,g2]).astype(np.float32)}
 
     for batch in range(ntrial // n_cores):
       trials = np.arange(batch*n_cores, (batch+1)*n_cores)
