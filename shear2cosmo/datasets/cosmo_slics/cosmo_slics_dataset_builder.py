bfc09a16ee920cb7b7d774bd6636a66162d8273a
Martinet Nicolas <nicolas.martinet@lam.fr>
2023-09-14 16:13:55 +0200

    building builder

--- shear2cosmo/datasets/cosmo_slics/cosmo_slics_dataset_builder.py
+++ shear2cosmo/datasets/cosmo_slics/cosmo_slics_dataset_builder.py
@@ -2,6 +2,15 @@
 
 import tensorflow_datasets as tfds
 
+def _get_Map(seed):
+    
+    
+    
+    
+    
+    
+    return seed, {'Map':full_image.array, 
+        'S8':S8.astype(np.float32)}
 
 class Builder(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for cosmo_slics dataset."""
@@ -17,31 +26,42 @@ class Builder(tfds.core.GeneratorBasedBuilder):
     return self.dataset_info_from_configs(
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
-            'image': tfds.features.Image(shape=(None, None, 3)),
-            'label': tfds.features.ClassLabel(names=['no', 'yes']),
+            #'e12': tfds.features.Tensor(shape=(512, 512, 2), dtype=np.float32),
+            'Map': tfds.features.Tensor(shape=(512, 512), dtype=np.float32),
+            #'CP': tfds.features.Tensor(shape=(4,), dtype=np.float32),
+            'S8': tfds.features.Tensor(shape=(1,), dtype=np.float32),
         }),
         # If there's a common (input, target) tuple from the
         # features, specify them here. They'll be used if
         # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('image', 'label'),  # Set to `None` to disable
+        supervised_keys=('Map', 'S8'),  # Set to `None` to disable
         homepage='https://dataset-homepage/',
     )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     # TODO(cosmo_slics): Downloads the data and defines the splits
-    path = dl_manager.download_and_extract('https://todo-data-url')
+    #path = dl_manager.download_and_extract('https://todo-data-url')
 
     # TODO(cosmo_slics): Returns the Dict[split names, Iterator[Key, Example]]
     return {
-        'train': self._generate_examples(path / 'train_imgs'),
+        #'train': self._generate_examples(path / 'train_imgs'),
+        'train': self._generate_examples(),
     }
 
   def _generate_examples(self, path):
     """Yields examples."""
-    # TODO(cosmo_slics): Yields (key, example) tuples from the dataset
-    for f in path.glob('*.jpeg'):
-      yield 'key', {
-          'image': f,
-          'label': 'yes',
-      }
+    from multiprocessing import Pool, cpu_count
+    # get the number of logical cpu cores
+    n_cores = cpu_count()
+    pool = Pool(processes=n_cores)
+    ntrial = 100
+
+    # Generate all images at once 
+    results = pool.map(_get_Map, np.arange(ntrial))
+
+    # Done, closing pool
+    pool.close()
+
+    for trial, result in results:
+      yield int(trial), result
